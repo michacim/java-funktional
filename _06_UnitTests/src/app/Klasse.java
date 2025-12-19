c8084a38461b3d3d586dce77dcca1ae25f4b397c
Micha Schirmer <michajava8@gmail.com>
2025-12-19 11:36:54 +0100

    junit test definieren

---
+++ _06_UnitTests/src/app/Klasse.java
@@ -0,0 +1,55 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class Klasse{
+
+    /**
+     *
+     * @param a - eine Zahl
+     * @param b - eine Zahl
+     * @return größere von beiden Zahlen
+     */
+    public int max(int a, int b){
+        return a > b ? a:b;
+    }
+
+    /**
+     *
+     * @param arr - Zahlenfolge oder Array: 3,4,6,8
+     * @return größte ermittelte Zahl des Arrays
+     * @throws  IllegalArgumentException - null
+     */
+    public int max(int... arr){ // int[]
+        if(arr==null){
+            throw new IllegalArgumentException("Liste darf nicht null sein!");
+        }
+        int max = arr[0];
+        for(int n:arr){
+            if(n > max){
+                max = n;
+            }
+        }
+        return max;
+    }
+
+    /**
+     *
+     * @param s a für Array, v für Vector
+     * @return new ArrayList, new Vector or null
+     * @param <T> beliebiger Typo
+     */
+    public <T>  List<T> createList(String s){ //<T> Platzhalter für beliebigen Datentyp
+        if(s.equalsIgnoreCase("a")){
+            return new ArrayList<>();
+        } else if (s.equalsIgnoreCase("v")) {
+            return new Vector<>();
+        }
+        return null;
+    }
+
+
+
+}
---
+++ _06_UnitTests/test/app/KlasseTest.java
@@ -0,0 +1,58 @@
+package app;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KlasseTest {
+
+    private Klasse klasse;
+
+
+    @BeforeEach
+    void setUp() {
+        klasse = new Klasse();
+    }
+
+    @Test
+    void max() {
+        int result = klasse.max(2,3);
+        assertEquals(3,result);
+        assertEquals(-1,klasse.max(-1,-3));
+
+
+    }
+
+    @Test
+    void testMax() {
+       int result =  klasse.max(23,1,3,-5,0,24,1);
+       assertEquals(24,result);
+
+
+       // Test von Exception die von einer Methode geworfen werden
+       assertThrows(IllegalArgumentException.class, () ->  {
+           klasse.max(null);
+       } );
+
+    }
+
+    @Test
+    void createList() {
+       List<String> arraylist=  klasse.createList("a");
+       assertNotNull(arraylist);
+       assertInstanceOf(ArrayList.class, arraylist);
+
+       List<String> vector=  klasse.createList("v");
+       assertNotNull(vector);
+       assertInstanceOf(Vector.class, vector);
+
+       List<Integer> nullList = klasse.createList("x");
+       assertNull(nullList);
+
+    }
+}
